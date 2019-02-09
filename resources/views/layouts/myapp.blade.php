41deda8da981110bbb1c79c1ff9c3e404e6fbeef
kair4ik <kair.21@mail.ru>
2019-02-09 17:31:28 +0600

    Добавил пару ссылок Создал файл для миграции

--- resources/views/layouts/myapp.blade.php
+++ resources/views/layouts/myapp.blade.php
@@ -16,6 +16,12 @@
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name', 'Laravel') }}
         </a>
+        <a class="navbar-brand" href="{{ url('/home') }}">
+            Список
+        </a>
+        <a class="navbar-brand" href="{{ url('/create') }}">
+            Создание
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>

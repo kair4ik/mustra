6be1a259e6a16754a19595c9346b2015a861a5d7
kair4ik <kair.21@mail.ru>
2019-02-10 02:49:36 +0600

    Надо теперь чтобы по нажатию менялся статус

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -37,7 +37,7 @@
                                 @endphp
                                     <td>
                                     @if(!empty($taskCur))
-                                    <button type="button" class="btn btn-{{\App\Task::getDescBy($taskCur[0]->status_id)}}">.</button>
+                                    <button type="button" class="btn btn-{{\App\Task::getDescBy($taskCur[0]->status_id)}}">о</button>
                                     @endif
                                     </td>
                                 @endforeach

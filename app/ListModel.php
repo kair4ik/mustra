3aa35fbbafad807ed59c6c01576025aed6d81040
kair4ik <kair.21@mail.ru>
2019-02-10 12:34:04 +0600

    Поправляем

--- app/ListModel.php
+++ app/ListModel.php
@@ -12,7 +12,7 @@ class ListModel extends Model
 
     public function getDayFromDate($days)
     {
-        return date('Y-m-d', strtotime($this->date_start. ' + '.$days.' days'));
+        return date('Y-m-d H:i:s', strtotime($this->date_start. ' + '.$days.' days'));
     }
 
 }
